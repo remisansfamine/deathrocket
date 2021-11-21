c8a029af0cf1875742714f254b2715d623dc56f5
Lancelot Bernard-Lepine <l.bernardlepine@student.isartdigital.com>
2021-11-21 23:31:42 +0100

    anim character

--- DeathRocket_Proto/Source/DeathRocket_Proto/DeathRocket_ProtoCharacter.h
+++ DeathRocket_Proto/Source/DeathRocket_Proto/DeathRocket_ProtoCharacter.h
@@ -6,9 +6,6 @@
 #include "GameFramework/Character.h"
 #include "DeathRocket_ProtoCharacter.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE(FHealthEvent);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE(FAmmoEvent);
-
 UCLASS(config=Game)
 class ADeathRocket_ProtoCharacter : public ACharacter
 {
@@ -22,138 +19,58 @@ class ADeathRocket_ProtoCharacter : public ACharacter
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	class UCameraComponent* FollowCamera;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Items, meta = (AllowPrivateAccess = "true"))
+		USkeletalMeshComponent* RocketLuncher;
+
 public:
 	ADeathRocket_ProtoCharacter();
-	~ADeathRocket_ProtoCharacter();
 
 	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
 	float BaseTurnRate;
 
 	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
 	float BaseLookUpRate;
 
 protected:
+
 	UPROPERTY(EditDefaultsOnly)
 	TSubclassOf<class ARocket> rocketClass;
 
 	void MoveForward(float Value);
 	void MoveRight(float Value);
-	void Jump();
 
 	void TurnAtRate(float Rate);
 	void LookUpAtRate(float Rate);
 
-	// ROCKET LAUNCHER
-	bool firing = false;
-	bool reloading = false;
-
-	UPROPERTY(EditAnywhere, Category = Weapon)
-	float fireRate = 1.f;
-	UPROPERTY(EditAnywhere, Category = Weapon)
-	float reloadTime = 5.f;
-
-	class Timer* fireTimer;
-	class Timer* reloadTimer;
-	// for the UI
-	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-	float fireProgress = 0.f;
-	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-	float reloadProgress = 0.f;
-
 	void Fire();
-	void EndFire();
-	void Reload();
-	void EndReload();
-	void UpdateTimersProgress();
 
-	// CAMERA
 	int   shoulder = 1;
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	float cameraYOffset = 30.f;
 	// Aim Down Sight (when aiming)
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	float ads = 50.f;
-	// Run Down Sight (when running)
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	float rds = 80.f;
 	// BASIC Field of view (when not aiming)
-	float fov;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	float fov = 90.f;
+
 	float curFov;
 
-	// HEALTH
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Health, meta = (AllowPrivateAccess = "true"))
-	int   healthMax = 2;
-	int   curHealth;
-	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-	float healthRatio = 1.f;
-
-	UPROPERTY(BlueprintAssignable, Category = Event)
-	FHealthEvent OnHealthUpdate;
-
-	// SPRINT
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
-	bool dashActivate = true;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
-	float walkingSpeed = 600.f;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
-	float runningSpeed = 1000.f;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
-	float dashingSpeed = 10000.f;
-
-	bool  sprinting = false;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
-	float dashMaxTime = 0.05f;
-	float curSprintTime = 0.f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
-	float dashRecoveryTime = 1.f;
-	bool dashRecovering = false;
-	class Timer* dashRecoveryTimer;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
-	float maxStamina = 100.f;
-	float curStamina;
-	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-	float staminaRatio = 1.f;
-	bool staminaRecup = false;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
-	float consumptionSeconds = 40.f;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
-	float recuperationSeconds = 30.f;
-
-	// AMMUNITION
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Weapon, meta = (AllowPrivateAccess = "true"))
-	int   ammoMax = 3;
-	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-	int   curAmmo;
-
-	UPROPERTY(BlueprintAssignable, Category = Event)
-	FAmmoEvent OnAmmoUpdate;
-
-	// METHODS
 	void changeCamSide();
 	void Aim();
 	void StopAiming();
-
-	void TakeDamage();
-	void Die();
-
-	void Sprint();
-	void StopSprint();
-	void RecoverDash();
-
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 	virtual void BeginPlay() override;
 
 public:
+
 	virtual void Tick(float DeltaTime) override;
 
 	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
 };
+
