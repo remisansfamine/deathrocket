023fecfb02dfedfda1580bba5f23668f67faed16
f.marcellot <f.marcellot@student.isartdigital.com>
2021-11-22 10:53:22 +0100

    [sprint] Upgrade sprint management :
    - Change fov when running
    - Press SHIFT when don't move doesn't consume stamina
    - Rename variable
    - Run until stamina = 0 force the player to wait for full stamina recovery
    
    - Add Reset() in Timer class

--- DeathRocket_Proto/Source/DeathRocket_Proto/Timer.h
+++ DeathRocket_Proto/Source/DeathRocket_Proto/Timer.h
@@ -17,6 +17,8 @@ public:
 	void Clear();
 	template< class UserClass >
 	void Set(UserClass* obj, typename FTimerDelegate::TUObjectMethodDelegate< UserClass >::FMethodPtr InTimerMethod);
+	template< class UserClass >
+	void Reset(UserClass* obj, typename FTimerDelegate::TUObjectMethodDelegate< UserClass >::FMethodPtr InTimerMethod);
 
 	float GetProgess();
 };
@@ -28,4 +30,11 @@ void Timer::Set(UserClass* obj, typename FTimerDelegate::TUObjectMethodDelegate<
 		return;
 
 	world->GetTimerManager().SetTimer(timer, obj, InTimerMethod, time, false);
+}
+
+template< class UserClass >
+void Timer::Reset(UserClass* obj, typename FTimerDelegate::TUObjectMethodDelegate< UserClass >::FMethodPtr InTimerMethod)
+{
+	Clear();
+	Set(obj, InTimerMethod);
 }
