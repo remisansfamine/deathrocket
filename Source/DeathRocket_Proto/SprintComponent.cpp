03fcc7b4bb02047d2a0270088f14ac5843843a0e
f.marcellot <f.marcellot@student.isartdigital.com>
2021-11-23 15:47:56 +0100

    [sprint] Migrate sprint params in new component

--- Source/DeathRocket_Proto/SprintComponent.cpp
+++ Source/DeathRocket_Proto/SprintComponent.cpp
@@ -1,9 +1,18 @@
 #include "SprintComponent.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Timer.h"
+
 // Sets default values for this component's properties
 USprintComponent::USprintComponent()
 {
-	PrimaryComponentTick.bCanEverTick = true;
+	//PrimaryComponentTick.bCanEverTick = true;
+	curStamina = maxStamina;
+}
+
+USprintComponent::~USprintComponent()
+{
+	delete dashRecoveryTimer;
 }
 
 
@@ -11,7 +20,8 @@ USprintComponent::USprintComponent()
 void USprintComponent::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	dashRecoveryTimer = new Timer(GetWorld(), dashRecoveryTime);
 }
 
 
@@ -19,5 +29,116 @@ void USprintComponent::BeginPlay()
 void USprintComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+void USprintComponent::TickStamina(float DeltaTime, bool isMoving)
+{
+	switch (state)
+	{
+	case ESprintState::RUN:
+		curSprintTime += DeltaTime;
+		curStamina -= runConsumptionSeconds * DeltaTime;
+
+		if (curStamina <= 0.f)
+		{
+			staminaRecup = true;
+			EndSprint();
+		}
+
+		break;
+
+	case ESprintState::DASH:
+		curSprintTime += DeltaTime;
+		curStamina -= dashConsumptionSeconds * DeltaTime;
+
+		if (curSprintTime >= dashMaxTime)
+			GoToRun();
+
+		break;
+
+	default: // WALK
+		curStamina = FMath::Min(curStamina + recoverySeconds * DeltaTime, maxStamina);
+		if (curStamina >= maxStamina)
+			staminaRecup = false;
+
+		break;
+	}
+
+	staminaRatio = curStamina / maxStamina;
+}
+
+float USprintComponent::GetSpeed(const ESprintState& s) const
+{
+	switch (s) {
+	case ESprintState::RUN:
+		return runningSpeed;
+	case ESprintState::DASH:
+		return dashingSpeed;
+	default:
+		return walkingSpeed;
+	}
+}
+
+float USprintComponent::GetSpeed() const
+{
+	return GetSpeed(state);
+}
+
+bool USprintComponent::CanSprint() const
+{
+	return !staminaRecup;
+}
+
+bool USprintComponent::CanDash() const
+{
+	return dashActivate && !dashRecovering;
+}
+
+void USprintComponent::Sprint()
+{
+	if (!CanSprint())
+		return;
+
+	curSprintTime = 0.f;
+
+	if (CanDash())
+		GoToDash();
+	else
+		GoToRun();
+
+	sprinting = true;
+
+}
+
+void USprintComponent::EndSprint()
+{
+	GoToWalk();
+}
+
+void USprintComponent::RecoverDash()
+{
+	dashRecovering = false;
+}
+
+void USprintComponent::GoToWalk()
+{
+	state = ESprintState::WALK;
+	OnEndRun.Broadcast();
+	sprinting = false;
+}
+
+void USprintComponent::GoToRun()
+{
+	state = ESprintState::RUN;
+	OnRun.Broadcast();
+}
+
+void USprintComponent::GoToDash()
+{
+	state = ESprintState::DASH;
+	OnDash.Broadcast();
+
+	dashRecovering = true;
+	dashRecoveryTimer->Reset(this, &USprintComponent::RecoverDash);
 }
 
--- Source/DeathRocket_Proto/SprintComponent.h
+++ Source/DeathRocket_Proto/SprintComponent.h
@@ -4,20 +4,90 @@
 #include "Components/ActorComponent.h"
 #include "SprintComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FDashDelegate);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FRunDelegate);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FEndRunDelegate);
+
+enum class ESprintState
+{
+	WALK = 0,
+	RUN = 1,
+	DASH = 2
+};
 
 UCLASS( ClassGroup=(Custom), Blueprintable, meta=(BlueprintSpawnableComponent) )
 class DEATHROCKET_PROTO_API USprintComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
-	USprintComponent();
-
 protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement|Dash", meta = (AllowPrivateAccess = "true"))
+		bool dashActivate = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement|Speed", meta = (AllowPrivateAccess = "true"))
+		float walkingSpeed = 600.f;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement|Speed", meta = (AllowPrivateAccess = "true"))
+		float runningSpeed = 800.f;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement|Speed", meta = (AllowPrivateAccess = "true"))
+		float dashingSpeed = 8000.f;
+
+	bool  sprinting = false;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement|Dash", meta = (AllowPrivateAccess = "true"))
+		float dashMaxTime = 0.05f;
+	float curSprintTime = 0.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement|Dash", meta = (AllowPrivateAccess = "true"))
+		float dashRecoveryTime = 1.f;
+	bool dashRecovering = false;
+	class Timer* dashRecoveryTimer;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement|Stamina", meta = (AllowPrivateAccess = "true"))
+		float maxStamina = 100.f;
+	float curStamina;
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+		float staminaRatio = 1.f;
+	bool staminaRecup = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement|Stamina", meta = (AllowPrivateAccess = "true"))
+		float runConsumptionSeconds = 40.f;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement|Stamina", meta = (AllowPrivateAccess = "true"))
+		float dashConsumptionSeconds = 100.f;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement|Stamina", meta = (AllowPrivateAccess = "true"))
+		float recoverySeconds = 30.f;
+	
 	virtual void BeginPlay() override;
 
-public:	
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	void RecoverDash();
 
-		
+	void GoToWalk();
+	void GoToRun();
+	void GoToDash();
+
+public:	
+	ESprintState state = ESprintState::WALK;
+
+	USprintComponent();
+	~USprintComponent();
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	void TickStamina(float DeltaTime, bool isMoving);
+
+	float GetSpeed(const ESprintState& state) const;
+	float GetSpeed() const;
+
+	bool CanSprint() const;
+	bool CanDash() const;
+	void Sprint();
+	void EndSprint();
+
+	UPROPERTY(BlueprintAssignable, Category = "Components|Run")
+	FDashDelegate OnDash;
+	
+	UPROPERTY(BlueprintAssignable, Category = "Components|Run")
+	FRunDelegate OnRun;
+
+	UPROPERTY(BlueprintAssignable, Category = "Components|Run")
+	FEndRunDelegate OnEndRun;
+
+	FORCEINLINE bool IsSprinting() const { return sprinting; }
 };
