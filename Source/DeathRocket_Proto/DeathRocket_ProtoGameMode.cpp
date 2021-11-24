06a59d0756953581075d458ebd091b02d14a1c2e
r.giner <remi.giner@hotmail.fr>
2021-11-24 16:06:34 +0100

    [playerspawn] Updating player spawn
    Moving player spawning from the GameMode to an entity made for it, adding respawn mecanic

--- Source/DeathRocket_Proto/DeathRocket_ProtoGameMode.cpp
+++ Source/DeathRocket_Proto/DeathRocket_ProtoGameMode.cpp
@@ -3,9 +3,6 @@
 #include "DeathRocket_ProtoGameMode.h"
 #include "DeathRocket_ProtoCharacter.h"
 #include "UObject/ConstructorHelpers.h"
-#include "Kismet/GameplayStatics.h"
-#include "GameFramework/PlayerStart.h"
-#include "GameFramework/PlayerController.h"
 
 ADeathRocket_ProtoGameMode::ADeathRocket_ProtoGameMode()
 {
@@ -16,40 +13,4 @@ ADeathRocket_ProtoGameMode::ADeathRocket_ProtoGameMode()
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
 
-	FGameModeEvents::GameModePostLoginEvent.AddUObject(this, &ADeathRocket_ProtoGameMode::OnSpawnPlayer);
-}
-
-void ADeathRocket_ProtoGameMode::StartPlay()
-{
-	Super::StartPlay();
-
-	// Get all player starts
-	TArray<AActor*> actors;
-	UGameplayStatics::GetAllActorsOfClass(GetWorld(), APlayerStart::StaticClass(), actors);
-
-	// Save them in memory
-	for (AActor* actor : actors)
-		playerStarts.Add(Cast<APlayerStart>(actor));
-}
-
-void ADeathRocket_ProtoGameMode::SpawnControllerAtPlayerStart(APlayerController* controller)
-{
-	// Get the last used player start index 
-	int playerStartIndex = lastPlayerStart % playerStarts.Num();
-
-	// Set the current transform with the player start transform
-	APlayerStart* currentStart = playerStarts[playerStartIndex];
-
-	controller->AcknowledgedPawn->SetActorTransform(currentStart->GetActorTransform());
-}
-
-void ADeathRocket_ProtoGameMode::OnSpawnPlayer(AGameModeBase* gamemode, APlayerController* controller)
-{
-	// Check if the current controller is not already registered
-	if (controllers.Find(controller))
-		return;
-
-	controllers.Add(controller);
-
-	SpawnControllerAtPlayerStart(controller);
 }
