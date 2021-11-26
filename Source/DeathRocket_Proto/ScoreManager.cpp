a66d53a4caa1cac8ef75f253ec2be044df5b88c4
Jarod SENGKEO <j.sengkeo@student.isartdigital.com>
2021-11-26 14:23:52 +0100

    [score] Score manager is independant from the GameMode
    - GameMode no longer initialize the score manager of the map
    - score manager should no longer be placed by hand on the map
    - the level blueprint spawns the score manager after creating every players
    - the score manager initialize itself

--- Source/DeathRocket_Proto/ScoreManager.cpp
+++ Source/DeathRocket_Proto/ScoreManager.cpp
@@ -7,11 +7,8 @@
 AScoreManager::AScoreManager()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-}
+	PrimaryActorTick.bCanEverTick = false;
 
-void AScoreManager::Init()
-{
 	// Get all player starts
 	TArray<AActor*> actors;
 	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ADeathRocket_ProtoCharacter::StaticClass(), actors);
@@ -35,10 +32,4 @@ const TArray<AActor*>& AScoreManager::GetEveryPlayers() const
 void AScoreManager::BeginPlay()
 {
 	Super::BeginPlay();
-}
-
-// Called every frame
-void AScoreManager::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
 }
--- Source/DeathRocket_Proto/ScoreManager.h
+++ Source/DeathRocket_Proto/ScoreManager.h
@@ -17,16 +17,10 @@ public:
 	// Sets default values for this actor's properties
 	AScoreManager();
 
-	void Init();
-
 	UFUNCTION(BlueprintCallable)
 	const TArray<AActor*>& GetEveryPlayers() const;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 };
