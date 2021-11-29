5a14733e5ba981ccc8c7fcc80c544f8cddec4e23
f.marcellot <f.marcellot@student.isartdigital.com>
2021-11-29 16:04:40 +0100

    [menu] Create menu level

---
+++ DeathRocket_Proto/Source/DeathRocket_Proto/MenuGameMode.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MenuGameMode.generated.h"
+
+UCLASS()
+class DEATHROCKET_PROTO_API AMenuGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	AMenuGameMode();
+};
